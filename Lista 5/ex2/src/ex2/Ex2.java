bf16f8f00b4232f90e486177e9ed094c9a371458
Vinícius Silva Montanari <vinicius_montanari@hotmail.com>
2020-10-18 17:19:11 -0300

    Ex1 e ex2 readicionados

---
+++ Lista 5/ex2/src/ex2/Ex2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex2;
+
+/**
+ *
+ * @author Montanari
+ */
+public class Ex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DynamicMatrix mat = new DynamicMatrix(4,4);
+        mat.print();
+        mat.fillAll();
+        mat.print();
+        
+        int[] casoTeste = {-1,1,1,1,2,3};
+        
+        for(int i=0;i<6;i+=3){
+
+            try{
+                mat.fillPos(casoTeste[i], casoTeste[i+1], casoTeste[i+2]);
+            }catch(ArrayIndexOutOfBoundsException e){
+                System.out.println(e.getMessage());
+                System.out.println();  
+            }
+            mat.print();
+        }
+       
+    }
+}
